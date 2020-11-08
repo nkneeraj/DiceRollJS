e9540eb21596ecc5d110b4365e86ac516a4f0e85
nkneeraj <nkneerj641@gmail.com>
2020-11-09 00:56:10 +0530

    [NK]:Number getting count of 10 first.

--- RollDice.js
+++ RollDice.js
@@ -1,17 +1,17 @@
 
     var map = new Map();
-
-    var count = 10;
-    while(count!=0){
-    let random = Math.floor(Math.random()*10)%6+1;
+    var random = 0;
+    let count = 10;
+    while(map.get(random)!=count){
+    random = Math.floor(Math.random()*10)%6+1;
     if(map.has(random))
         map.set(random, map.get(random)+1);
     else{
         map.set(random,1);
         console.log("key set.");
     }
-    console.log("Random dice roll value: " +random);
-    count--;
+    // console.log("Random dice roll value: " +random);
     }
+    console.log("Dice value having count 10 is: "+random);
 
 
